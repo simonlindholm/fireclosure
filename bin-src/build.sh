d41ca6f59396c18cb4fedca9c4ffbc3dcef265fd
Simon Lindholm <simon.lindholm10@gmail.com>
2012-03-07 23:24:31 +0100

    Link to the XPCOM glue.

--- bin-src/build.sh
+++ bin-src/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-g++ -c -fPIC util.cpp module.cpp -I /usr/include/firefox-12.0a2/
-gcc -shared -Wl,-soname,fcutils.so -o fcutils.so util.o module.o
+IP="/usr/include/firefox-12.0a2"
+LP="/usr/lib/firefox-devel-12.0a2"
+g++ -g -c -fPIC util.cpp module.cpp -I $IP -include "xpcom-config.h"
+gcc -shared -Wl,-soname,fcutils.so -o fcutils.so util.o module.o -L"$LP/sdk/lib" -L"$LP/sdk/bin" -Wl,-rpath-link,"$LP/sdk/bin" -lxpcomglue_s -lxpcom -lnspr4 -lmozalloc
 mkdir -p ../platform/Linux_x86-gcc3/components/
 mv fcutils.so ../platform/Linux_x86-gcc3/components/
 cp iutil.xpt ../components/
