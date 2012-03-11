13db30ce8b1da33c541a8b36b26e60b57cb69c8d
Simon Lindholm <simon.lindholm10@gmail.com>
2012-03-11 17:12:06 +0100

    Changed paths in build script.

--- bin-src/build.sh
+++ bin-src/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-IP="/usr/include/firefox-12.0a2"
-LP="/usr/lib/firefox-devel-12.0a2"
+IP="/usr/include/firefox"
+LP="/usr/lib/firefox-devel"
 g++ -g -c -fPIC util.cpp module.cpp -I $IP -include "xpcom-config.h"
 gcc -shared -Wl,-soname,fcutils.so -o fcutils.so util.o module.o -L"$LP/sdk/lib" -L"$LP/sdk/bin" -Wl,-rpath-link,"$LP/sdk/bin" -lxpcomglue_s -lxpcom -lnspr4 -lmozalloc
 mkdir -p ../platform/Linux_x86-gcc3/components/
--- bin-src/gen.sh
+++ bin-src/gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python /usr/lib/firefox-devel-12.0a2/sdk/bin/header.py --cachedir=. -o iutil.h iutil.idl -I /usr/share/idl/firefox-12.0a2/
-python /usr/lib/firefox-devel-12.0a2/sdk/bin/typelib.py --cachedir=. -o iutil.xpt iutil.idl -I /usr/share/idl/firefox-12.0a2/
+python /usr/lib/firefox-devel/sdk/bin/header.py --cachedir=. -o iutil.h iutil.idl -I /usr/share/idl/firefox/
+python /usr/lib/firefox-devel/sdk/bin/typelib.py --cachedir=. -o iutil.xpt iutil.idl -I /usr/share/idl/firefox/
