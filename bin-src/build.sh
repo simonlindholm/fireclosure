ffb37a8b39ca0192ad1a3f2a37303c082494f02f
Simon Lindholm <simon.lindholm10@gmail.com>
2012-03-13 00:36:02 +0100

    Build specifically for 12.0a2.

--- bin-src/build.sh
+++ bin-src/build.sh
@@ -3,6 +3,6 @@ IP="/usr/include/firefox"
 LP="/usr/lib/firefox-devel"
 g++ -g -c -fPIC util.cpp module.cpp -I $IP -include "xpcom-config.h"
 gcc -shared -Wl,-soname,fcutils.so -o fcutils.so util.o module.o -L"$LP/sdk/lib" -L"$LP/sdk/bin" -Wl,-rpath-link,"$LP/sdk/bin" -lxpcomglue_s -lxpcom -lnspr4 -lmozalloc
-mkdir -p ../platform/Linux_x86-gcc3/components/
-mv fcutils.so ../platform/Linux_x86-gcc3/components/
 cp iutil.xpt ../components/
+mkdir -p ../components/Linux_x86-gcc3/12.0a2/
+mv fcutils.so ../components/Linux_x86-gcc3/12.0a2/
