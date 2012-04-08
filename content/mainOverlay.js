9984d8766a90fbdbef421150c23794b522dc7763
Simon Lindholm <simon.lindholm10@gmail.com>
2012-04-08 21:06:56 +0200

    Make it restartless again.

--- content/mainOverlay.js
+++
@@ -1,9 +0,0 @@
-/* See license.txt for terms of usage */
-
-// Register trace listener the customizes trace logs coming from this extension.
-Firebug.registerTracePrefix("FireClosure;", "DBG_FIRECLOSURE", true,
-    "chrome://fireclosure/skin/fireclosure.css");
-
-// Register the extension. The 'main' module is loaded automatically.
-var config = {id: "fireclosure@simonsoftware.se"};
-Firebug.registerExtension("fireclosure", config);
